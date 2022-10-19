d1df531b48462294420b2f0923c2d0dbb81e31c3
LucaFrioli <lucafrioli7@gmail.com>
2022-10-19 02:48:39 -0300

    Add First External JS Page

---
+++ estudo/JavaScript_Estudo/CursoEmVideo/Aula07/AnimatedCanvas.js
@@ -0,0 +1,11 @@
+
+var x = window.prompt(`Olá digite seu Nome:`)
+var z = x
+var Congrat = ` esperamos que esteja gostando!!`
+function interactive() {
+    let y =`Este botão pode mudar todo e qualquer <br> texto que está definido na programação`
+    x = y
+    window.alert(`${z} muito obrigado por estar testando o nosso site!!`)
+    document.getElementById(`teste`).innerHTML = this.x.toUpperCase()
+    document.getElementById(`Nem`).innerHTML =  z+Congrat
+}
