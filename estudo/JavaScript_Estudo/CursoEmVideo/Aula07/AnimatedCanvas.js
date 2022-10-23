75c33492162702470e878d472713b95d4448f45a
LucaFrioli <lucafrioli7@gmail.com>
2022-10-23 12:56:43 -0300

    Inicialization of page DOM
    ADD inicial page of DOM studies
    and css3 code of then
    
    +fine adjustments

--- estudo/JavaScript_Estudo/CursoEmVideo/Aula07/AnimatedCanvas.js
+++ estudo/JavaScript_Estudo/CursoEmVideo/Aula07/AnimatedCanvas.js
@@ -1,11 +1,23 @@
 
 var x = window.prompt(`Olá digite seu Nome:`)
-var z = x
+var age = window.prompt(`Por favor digite também sua idade:`)
 var Congrat = ` esperamos que esteja gostando!!`
+
 function interactive() {
-    let y =`Este botão pode mudar todo e qualquer <br> texto que está definido na programação`
-    x = y
-    window.alert(`${z} muito obrigado por estar testando o nosso site!!`)
-    document.getElementById(`teste`).innerHTML = this.x.toUpperCase()
-    document.getElementById(`Nem`).innerHTML =  z+Congrat
+    var Maturi = age<=17 ? `Menor`: `Maior`
+    document.getElementById(`titleLanding`).innerHTML =`Olá  ${x}  ${Congrat} <br> Identificamos que vc é ${Maturi} de idade!<br>Este site ainda está em processo de aprimoramento, pedimos perdão pelo transtorno.`
+    if(Maturi==`Menor`){
+        document.getElementById(`recomendation`).innerHTML = `${x} por  vc ser ${Maturi} de idade recomendamos fazer uma revisão completa das aulas anteriores!`
+    }else{
+        document.getElementById(`recomendation`).innerHTML = `${x} por  vc ser ${Maturi} de idade poderá seguir com as aulas normalmente, não esqueça de se exercítar!!`
+    }
+}
+
+function DataFacts(){
+    document.getElementById(`teste`).innerHTML = `Como vc já tem ${Year} dentor do mundo da programação,`
+    if(Year<2){
+        document.getElementById(`EJPS`).innerHTML = `Já poderá aplicar para vagas de estágio como DEV`
+    }else{
+        ((Year<=4) && (Year>=2)) ? document.getElementById(`EJPS`).innerHTML=`Já poderá aplicar em uma vaga de junior`:`Já poderá aplicar para uma vaga de Pleno`
+    }
 }
